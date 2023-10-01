87424168ab4ffb477945180e2ae5fe3e0da3722a
Adissuu <gabrieldubois.eng@gmail.com>
2023-10-01 12:30:50 -0400

    pushing

--- Extension/contentScript.js
+++ Extension/contentScript.js
@@ -1,4 +1,4 @@
-const url = "https://1632-37-120-244-62.ngrok-free.app"
+const url = "https://22c3-37-120-244-62.ngrok-free.app"
 
 const lines = function () {
   const text = document.body.innerText;
@@ -24,10 +24,53 @@ function httpGet(theUrl) {
   return xmlHttp.responseText;
 }
 
+function httpPost(theUrl, data, callback) {
+  return new Promise((resolve, reject) => {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("POST", url, true);
+    xmlHttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlHttp.setRequestHeader("ngrok-skip-browser-warning", "true");
+
+    xmlHttp.onreadystatechange = function () {
+      if (xmlHttp.readyState === XMLHttpRequest.DONE) {
+        if (xmlHttp.status === 200) {
+          resolve(JSON.parse(xmlHttp.responseText));
+        } else {
+          reject(`HTTP request failed with status ${xmlHttp.status}`);
+        }
+      }
+    };
+
+    xmlHttp.send(JSON.stringify(data));
+  });
+}
 
 let linesAll = lines()
-console.log(httpGet(`${url}/test_route`))
+const timeout = 30000; // 30 seconds
+let timeoutId;
+let data = {
+  name: 'genghis-khan',
+  passage_text: linesAll
+}
 
+const httpRequestPromise = httpPost(`${url}/vectorize_page`, data);
+const timeoutPromise = new Promise((resolve, reject) => {
+  timeoutId = setTimeout(() => {
+    clearTimeout(timeoutId);
+    reject('Request timed out');
+  }, timeout);
+});
+
+
+Promise.race([httpRequestPromise, timeoutPromise])
+  .then(response => {
+    clearTimeout(timeoutId);
+    console.log('Response:', response);
+  })
+  .catch(error => {
+    clearTimeout(timeoutId);
+    console.error('Error:', error);
+  });
 
 
 
