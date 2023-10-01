2dc54b899de600d95cde01fcbfa911112c1da8df
Adissuu <gabrieldubois.eng@gmail.com>
2023-09-30 22:03:54 -0400

    wat

--- contentScript.js
+++ contentScript.js
@@ -1,44 +1,84 @@
-import { Pinecone } from "@pinecone-database/pinecone";
+const pineconeScript = document.createElement('script');
+pineconeScript.src = 'https://unpkg.com/@pinecone-database/pinecone@0.3.0/dist/pinecone.min.js';
+document.head.appendChild(pineconeScript);
 
-const numberedLines = function () {
-  const text = document.body.innerText;
-  let numberedText = text.split('\n').map((line, index) => `${index + 1}. ${line}`).join('\n')
-  let numberedLines = []
+const tfjsScript = document.createElement('script');
+tfjsScript.src = 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.8.0/dist/tf.min.js';
+document.head.appendChild(tfjsScript);
 
-  numberedText.split('\n').forEach(line => {
+const tokenizerScript = document.createElement('script');
+tokenizerScript.src = 'https://cdn.jsdelivr.net/npm/@tensorflow-models/universal-sentence-encoder@4.0.0/dist/universal-sentence-encoder.min.js';
+document.head.appendChild(tokenizerScript);
 
-    if (line.length > 10) {
-      numberedLines.push(line)
+pineconeScript.onload = async function () {
+  tfjsScript.onload = async function () {
+    tokenizerScript.onload = async function () {
+      const Pinecone = window.Pinecone;
+      const loadTokenizer = window['universal-sentence-encoder'].loadTokenizer;
+      const numberedLines = function () {
+        const text = document.body.innerText;
+        let numberedText = text.split('\n').map((line, index) => `${index + 1}. ${line}`).join('\n')
+        let numberedLines = []
+
+        numberedText.split('\n').forEach(line => {
+
+          if (line.length > 10) {
+            numberedLines.push(line)
+          }
+        });
+
+
+        console.log(numberedLines)
+        return numberedLines
+      }
+
+
+      const name = document.head.title
+      const pinecone = new Pinecone();
+
+      await pinecone.init({
+        environment: "gcp-starter",
+        apiKey: "1ff3957c-24e8-4a00-9037-34a4fd541813",
+      });
+
+      await pinecone.createIndex({
+        name: name,
+        dimension: 512,
+        waitUntilReady: true,
+      });
+
+      await pinecone.describeIndex(name);
+      const index = pinecone.index(name)
+
+      let encodedLines = []
+
+      numberedLines.forEach(line => {
+        loadTokenizer().then(tokenizer => {
+          encodedLines.push(tokenizer.encode(line));
+        })
+      });
+
+      let count = 0
+      encodedLines.forEach(async encodedLine => {
+        await index.upsert([{
+          "id": count,
+          "values": encodedLine
+        }])
+        count++;
+      })
+
+      console.log(encodedLines)
+
+
+
     }
-  });
-
-  return numberedLines
+  }
 }
 
-const uuid = function () {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-    .replace(/[xy]/g, function (c) {
-      const r = Math.random() * 16 | 0,
-        v = c == 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-}
-
-const pinecone = new Pinecone();
-await pinecone.init({
-  environment: "gcp-starter",
-  apiKey: "1ff3957c-24e8-4a00-9037-34a4fd541813",
-});
-
-
-await pinecone.createIndex({
-  name: uuid,
-  dimension: numberedLines.length,
-  waitUntilReady: true,
-});
 
 
 
 
-const index = pinecone.Index("tutorial-question-answering");
+
+
 
