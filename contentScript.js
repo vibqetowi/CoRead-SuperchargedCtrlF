27b3ca8ce16950498af1d01ea85ee72c34544205
Adissuu <gabrieldubois.eng@gmail.com>
2023-09-30 22:11:25 -0400

    still broken

--- contentScript.js
+++ contentScript.js
@@ -2,16 +2,16 @@ const pineconeScript = document.createElement('script');
 pineconeScript.src = 'https://unpkg.com/@pinecone-database/pinecone@0.3.0/dist/pinecone.min.js';
 document.head.appendChild(pineconeScript);
 
-const tfjsScript = document.createElement('script');
-tfjsScript.src = 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.8.0/dist/tf.min.js';
-document.head.appendChild(tfjsScript);
-
-const tokenizerScript = document.createElement('script');
-tokenizerScript.src = 'https://cdn.jsdelivr.net/npm/@tensorflow-models/universal-sentence-encoder@4.0.0/dist/universal-sentence-encoder.min.js';
-document.head.appendChild(tokenizerScript);
-
 pineconeScript.onload = async function () {
+  const tfjsScript = document.createElement('script');
+  tfjsScript.src = 'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.8.0/dist/tf.min.js';
+  document.head.appendChild(tfjsScript);
+
   tfjsScript.onload = async function () {
+    const tokenizerScript = document.createElement('script');
+    tokenizerScript.src = 'https://cdn.jsdelivr.net/npm/@tensorflow-models/universal-sentence-encoder@4.0.0/dist/universal-sentence-encoder.min.js';
+    document.head.appendChild(tokenizerScript);
+
     tokenizerScript.onload = async function () {
       const Pinecone = window.Pinecone;
       const loadTokenizer = window['universal-sentence-encoder'].loadTokenizer;
